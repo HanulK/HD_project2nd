7afd3c038db9d8a91431330663b5a58da0db9f5b
skarns23 <skarns23@naver.com>
2022-12-30 09:41:50 +0900

    주문내역 ajax 처리 중

--- src/test/java/org/kabart/service/SearchOrderServiceTests.java
+++ src/test/java/org/kabart/service/SearchOrderServiceTests.java
@@ -10,14 +10,14 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/root-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/security-context.xml"})
 @Log4j
 public class SearchOrderServiceTests {
-	
-	@Setter(onMethod_=@Autowired)
+
+	@Setter(onMethod_ = @Autowired)
 	private SearchOrederService service;
-	
-	
+
 	@Test
 	public void testSearchOrder() {
 		String id = "test";
