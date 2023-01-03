5d0cb8b25bc773ceff87588bef2004713cf525c4
HanulKim <hanscnnee@gmail.com>
2023-01-03 18:04:41 +0900

    start : 회원 정보 수정

--- src/main/java/org/kabart/mapper/MemberMapper.java
+++ src/main/java/org/kabart/mapper/MemberMapper.java
@@ -4,6 +4,7 @@ import org.kabart.domain.MemberVO;
 
 public interface MemberMapper {
 	public MemberVO read(String mem_id);
+	public MemberVO getInfo(String mem_id);
 	public int insertMember(MemberVO member);
 	public void setAuth(String mem_id);
 	public int idCheck(String new_id);
