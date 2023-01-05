872ccd85df65e9cfb2b6f5fa2b12617edb222ba1
skarns23 <skarns23@naver.com>
2023-01-05 20:00:14 +0900

    주문하기 기능 구현 완

--- src/main/java/org/kabart/service/OrderServiceImpl.java
+++ src/main/java/org/kabart/service/OrderServiceImpl.java
@@ -1,11 +1,12 @@
 package org.kabart.service;
 
-import java.util.List;
+import java.util.*;
 
 import org.kabart.domain.OrderVO;
-import org.kabart.mapper.OrderMapper;
+import org.kabart.mapper.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -17,6 +18,9 @@ public class OrderServiceImpl implements OrderService{
 	@Setter(onMethod_= {@Autowired})
 	private OrderMapper oMapper;
 	
+	@Setter(onMethod_= {@Autowired})
+	private CartsMapper cMapper;
+	
 	@Override
 	public List<OrderVO> getProducts(List<String> pids,int isUsed) {
 		List<OrderVO> result;
@@ -25,6 +29,32 @@ public class OrderServiceImpl implements OrderService{
 		}else {
 			result = oMapper.getNewList(pids);
 		}
+		return result;
+	}
+
+	
+	/*
+	 * private int prod_id; private String prod_name; private int prod_price;
+	 * private String img_srcs; private String prod_category; private int quantity;
+	 * private int up_id; private String grade; private int discount_rate;
+	 */
+	@Transactional
+	@Override
+	public int insertProducts(String mem_id, String addr, String addr_dt, String phone, String dname,
+			 List<String> prod_id, List<String> quantity, String isUsed) {
+		oMapper.orderID(mem_id, addr, addr_dt, phone, dname);
+		int result = 0;
+		List<OrderVO> oVO = new ArrayList<>();
+		String review_check ="0";
+		if(isUsed.equals("1")) {
+			review_check = "1";
+		}
+		for(int i = 0 ;i<prod_id.size();i++) {
+			result += oMapper.insertOrders(mem_id, prod_id.get(i), quantity.get(i), isUsed, review_check);
+			cMapper.removeCart(mem_id, Integer.parseInt(prod_id.get(i)));
+		}
+		
+		
 		return result;
 	}
 
--- src/test/java/org/kabart/service/OrderServiceTests.java
+++ src/test/java/org/kabart/service/OrderServiceTests.java
@@ -30,7 +30,33 @@ public class OrderServiceTests {
 		list.add("149012");
 		int isUsed = 0;
 		oService.getProducts(list, isUsed).forEach(item -> log.info(item));
-		
+	}
 	
+	
+	/*
+	 * String mem_id, String addr, String addr_dt, String phone, String dname,
+	 * List<String> prod_id, List<String> quantity, String isUsed
+	 */
+	@Test
+	public void insertOrder() {
+		String mem_id = "dodo";
+		String addr = "!@#";
+		String addr_dt = "123";
+		String phone = "01051523372";
+		String dname = "햄뿡";
+		List<String> prod_id = new ArrayList<String>();
+		prod_id.add("146809");
+		prod_id.add("146811");
+		prod_id.add("149013");
+		prod_id.add("149012");
+		prod_id.add("108607");
+		List<String> quantity = new ArrayList<>();
+		quantity.add("3");
+		quantity.add("3");
+		quantity.add("3");
+		quantity.add("1");
+		quantity.add("2");
+		String isUsed="0";
+		log.info(oService.insertProducts(mem_id, addr, addr_dt, phone, dname, prod_id, quantity, isUsed));
 	}
 }
